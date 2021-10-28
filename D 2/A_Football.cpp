7b62220cca59cde2b09311865051644134069b8f
RADIUMz <abhi.radiumz@gmail.com>
2021-10-29 03:06:42 +0530

    push code

---
+++ D 2/A_Football.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define int long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define endl "\n"
+#define sp(n) fixed<<setprecision(n)
+#define countBits(x) __builtin_popcount(x)
+#define ctz(x) __builtin_ctz(x)// no. of unset bits before first setBit
+typedef pair<int, int>	pii;
+typedef vector<int>		vi;
+typedef vector<pii>		vpii;
+typedef vector<vi>		vvi;
+const int mod = 1'000'000'007;
+int mpow(int base, int exp);
+
+void solve(){
+    string s;
+    cin>>s;
+    int a=0,b=0;
+    for(int i=0;i<s.length();i++){
+
+        if(s[i]=='1'){
+            a++;
+            b=0;
+        }
+        if(s[i]=='0'){
+            b++;
+            a=0;
+        }
+
+        if(a==7 or b==7){
+            cout<<"YES";
+            return;
+        }
+    }
+    cout<<"NO";
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }   
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % mod;
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
---
+++ D 2/B_Jeff_and_Periods.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define int long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define endl "\n"
+#define sp(n) fixed << setprecision(n)
+#define countBits(x) __builtin_popcount(x)
+#define ctz(x) __builtin_ctz(x) // no. of unset bits before first setBit
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+const int mod = 1'000'000'007;
+vector<int> sieve(int n)
+{
+    int *arr = new int[n + 1]();
+    vector<int> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0)
+        {
+            vect.push_back(i);
+            for (int j = (i * i); j <= n; j += i)
+                arr[j] = 1;
+        }
+    return vect;
+}
+int mpow(int base, int exp)
+{
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+int mod_add(int a, int b, int m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+int mod_mul(int a, int b, int m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+int mod_sub(int a, int b, int m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    map<int, vi> m;
+    fo(i, n)
+    {
+        int x;
+        cin >> x;
+        m[x].pb(i);
+    }
+    map<int, int> ans;
+    for (auto x : m)
+    {
+        if (x.ss.size() == 1)
+            ans[x.ff] = 0;
+        else
+        {
+            bool f = true;
+            int diff = x.ss[1] - x.ss[0];
+            Fo(i, 1, x.ss.size())
+            {
+                int comp = x.ss[i] - x.ss[i - 1];
+                if (comp != diff)
+                {
+                    f = false;
+                    break;
+                }
+            }
+            if (f == true)
+            {
+                ans[x.ff] = diff;
+            }
+        }
+    }
+    cout << ans.size() << endl;
+    for (auto x : ans)
+    {
+        cout << x.ff << " " << x.ss << endl;
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+}
